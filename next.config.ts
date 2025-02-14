2b96e76949b85756ae248aa7ad02491680328bf1
Osadchyi <maksim.osadchyi@trident.software>
2025-02-14 13:57:15 +0200

    Tets

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 const nextConfig = {
     output: "export",
-    basePath: "/valentine-for-beloved",
-    assetPrefix: "/valentine-for-beloved/",
     trailingSlash: true,
     images: { unoptimized: true },
     distDir: "out", // Изменяем папку билда
