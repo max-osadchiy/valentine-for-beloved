88b47e2b739f24d74f27d6ec6c8e7b4ef5e95ab3
Osadchyi <maksim.osadchyi@trident.software>
2025-02-14 12:14:31 +0200

    Фикс деплоя

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'export',
     basePath: '/valentine-for-beloved', // Укажи имя репозитория, если сайт не на корневом домене
+    assetPrefix: "/valentine-for-beloved", // Правильные пути для статических файлов
     images: { unoptimized: true } // Next.js требует это для экспорта
 };
 
