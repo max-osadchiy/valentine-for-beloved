bca90dc80bd8fd26b748329a5585fa62015abb3d
Osadchyi <maksim.osadchyi@trident.software>
2025-02-14 13:47:06 +0200

    1

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-    output: 'export',
-    basePath: '/valentine-for-beloved', // Укажи имя репозитория, если сайт не на корневом домене
-    assetPrefix: "/valentine-for-beloved", // Правильные пути для статических файлов
-    images: { unoptimized: true } // Next.js требует это для экспорта
+const nextConfig = {
+    output: "export",
+    basePath: "/valentine-for-beloved",
+    assetPrefix: "/valentine-for-beloved/",
+    trailingSlash: true,
+    images: { unoptimized: true },
+    distDir: "out", // Изменяем папку билда
 };
 
 export default nextConfig;
